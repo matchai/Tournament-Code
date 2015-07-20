a25a4c8ad69383159b9451249e1ae3bca84b0275
matchai <matchai@me.com>
2015-07-20 16:28:38 +0200

    Replaced interaction classes with active and inactive

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
-var id;
+/*------------------------------------*\
+    UI Interaction
+\*------------------------------------*/
 
+// Input label animation
+var id;
 $('input').keyup(function() {
   id = $(this).attr('id');
   if ($(this).val()) {
-    $('label[for="' + id + '"]').addClass('labelEnabled');
-    $('label[for="' + id + '"]').removeClass('labelDisabled');
+    $('label[for="' + id + '"]').addClass('active');
+    $('label[for="' + id + '"]').removeClass('inactive');
   } else {
-    $('label[for="' + id + '"]').addClass('labelDisabled');
-    $('label[for="' + id + '"]').removeClass('labelEnabled');
+    $('label[for="' + id + '"]').addClass('inactive');
+    $('label[for="' + id + '"]').removeClass('active');
   }
+});
+
+// Advanced pane
+$('.customize').click(function() {
+  $(this).addClass('active');
 });
