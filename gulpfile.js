94fb4f754d966183c4c1f7ba0d598d131b45de4d
matchai <matchai@me.com>
2015-08-18 12:36:00 +0000

    Added ghpages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,51 @@
 var gulp = require('gulp');
 
-// CSS Packages
+// CSS packages
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require ('gulp-cssmin');
 
-// JS Packages
+// JS packages
 var uglify = require('gulp-uglify');
 
-// General Packages
+// General packages
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var deploy = require('gulp-deploy-git');
+
 
 var copyFiles = [
+        './CNAME',
         './index.html',
         './fonts/**/*',
         './img/**/*'
     ]
 
 
-// CSS Tasks
+// CSS tasks
 gulp.task('css', function() {
-    return gulp.src("stylesheets/**/*.scss")
+    return gulp.src("./stylesheets/**/*.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe (cssmin())
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
 
-// JS Tasks
+// JS tasks
 gulp.task('js', function() {
-    return gulp.src('scripts/**/*.js')
+    return gulp.src('./scripts/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts'))
+        .pipe(gulp.dest('./dist/scripts'))
 });
 
 
 // Watch files for changes
 gulp.task('watch', ['serve'], function() {
-    gulp.watch('*.html', ['copy']).on('change', reload);
-    gulp.watch('stylesheets/**/*.scss', ['css']);
-    gulp.watch('scripts/**/*.js'), ['js'];
+    gulp.watch('./*.html', ['copy']).on('change', reload);
+    gulp.watch('./stylesheets/**/*.scss', ['css']);
+    gulp.watch('./scripts/**/*.js'), ['js'], reload;
 });
 
 
@@ -58,8 +61,18 @@ gulp.task('serve', ['css'], function() {
 // Copy assets to dist
 gulp.task('copy', function() {
     gulp.src(copyFiles, { base: './' })
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 })
+
+// Deploy task
+gulp.task('deploy', ['copy', 'css', 'js'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(deploy({
+      repository: 'https://github.com/matchai/Tournament-Code.git',
+      branches: ['ghpages']
+    }));
+});
+
 
 // Default task
 gulp.task('default', ['copy', 'css', 'js', 'serve']);
